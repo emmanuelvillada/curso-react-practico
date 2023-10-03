fdf16901a5db9da5dbfa43e085bdb720251644c0
Emmanuel Villada Suarez <emmanuelvillada310016@correo.itm.edu.co>
2023-10-03 11:52:13 -0500

    order card with delete function

--- src/components/OrderCArd/index.jsx
+++ src/components/OrderCArd/index.jsx
@@ -13,7 +13,7 @@ function OrderCard(props) {
             </div>
             <div className='flex items-center gap-2'>
                 <p className='text-lg font-medium'>{price}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <svg onClick={() =>handleDelete(id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 </svg>
 
