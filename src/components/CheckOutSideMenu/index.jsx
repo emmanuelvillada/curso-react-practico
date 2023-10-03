d265dae1e8aae4300fdde1041f4b593d2bb65ab9
Emmanuel Villada Suarez <emmanuelvillada310016@correo.itm.edu.co>
2023-10-03 10:54:25 -0500

    add the my orders function

--- src/components/CheckOutSideMenu/index.jsx
+++ src/components/CheckOutSideMenu/index.jsx
@@ -5,6 +5,11 @@ import { totalPrice } from "../../Utils";
 function CheckOutSideMenu() {
     const context = useContext(ShoppingCartContext)
 
+    const handleDelete = (id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id)
+        context.setCartProducts(filteredProducts)
+    }
+
 
     return (
         <aside
@@ -13,8 +18,8 @@ function CheckOutSideMenu() {
                 <h2 className='font-medium text-xl'>My Order</h2>
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer"
-                    onClick= {() => Context.closeProductDetail}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        onClick={() => Context.closeProductDetail()}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </div>
             </div>
@@ -32,9 +37,10 @@ function CheckOutSideMenu() {
             </div>
             <div className="px-6">
                 <p className="flex justify-between items-center">
-                    <span>Precio total:</span>
-                    <span>${totalPrice(context.cartProducts)}</span>
+                    <span className="font-light">Precio total:</span>
+                    <span className="font-medium text-2xl">${totalPrice(context.cartProducts)}</span>
                 </p>
+                <button onClick={() => handleCheckout()}>Checkout</button>
             </div>
         </aside>
     )
